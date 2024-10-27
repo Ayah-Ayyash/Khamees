a5b0f65636e32b023498332d17f5096885640a45
Ayah-Ayyash <ayahayyash710@gmail.com>
2024-10-27 13:21:15 +0200

    url

--- Assets/scripts/Khamees.cs
+++ Assets/scripts/Khamees.cs
@@ -7,7 +7,9 @@ public class Khamees : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Ayah-Ayyash");
+        Debug.Log("Ayah-Ayyash");       
+         Debug.Log("Ayah-Ayyash");
+
     }
 
     // Update is called once per frame
