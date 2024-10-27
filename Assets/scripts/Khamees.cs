9f7b03c4f502ff01234f4ec8d2a0218f6b884922
Ayah-Ayyash <ayahayyash710@gmail.com>
2024-10-27 15:46:44 +0200

    Ass

--- Assets/scripts/Khamees.cs
+++ Assets/scripts/Khamees.cs
@@ -7,36 +7,17 @@ public class Khamees : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
-        // string FirstName="Ayah";
-        // string LastName="Ayyash";
-
-        // Debug.Log(FirstName.Length);
-
-        // if ((5 + 5) + 3 == 12)
-        // {
-        //     Debug.Log("winer");
-
-
-        // }
-        // else {
-        //     Debug.Log("loser");
-
-        // }
-
-    int count=0;
-    while(count<=5){
-        int RandomNumber=Random.Range(1,5);
-        Debug.Log(RandomNumber);
-        count++;
-        if(RandomNumber == 2) break;
-        Debug.Log("Break");
+        int count = 0;
+        while (count < 20)
+        {
+            int RandomCount = Random.Range(1, 21);
+            Debug.Log(RandomCount);
+            count++;
+            if (RandomCount == 5) continue;
+            Debug.Log("Loser");
+            if (RandomCount == 15) break;
+            Debug.Log("LoserAgain");
+        }
     }
-
-
-
-
-    }
-
 }
 
